7e4cea0e591c1d076e6a197945cc81020e491786
Tim Lai <timlaicodes@gmail.com>
2025-03-07 19:49:11 -0500

    Added Content component to App component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import Content from "./components/Content.vue";
 </script>
 
 <template>
   <Header />
-  <main class="min-h-[80vh]"></main>
+  <Content />
   <Footer />
 </template>
 
