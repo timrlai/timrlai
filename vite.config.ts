cd8e22f18fde4070a9ac2f97f60bacf38d1d99e4
Tim Lai <timlaicodes@gmail.com>
2025-03-14 09:41:25 -0400

    Added glsl config to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import glsl from "vite-plugin-glsl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    glsl({
+      include: [
+        // Glob pattern, or array of glob patterns to import
+        "**/*.glsl",
+        "**/*.wgsl",
+        "**/*.vert",
+        "**/*.frag",
+        "**/*.vs",
+        "**/*.fs",
+      ],
+      exclude: undefined, // Glob pattern, or array of glob patterns to ignore
+      warnDuplicatedImports: true, // Warn if the same chunk was imported multiple times
+      removeDuplicatedImports: false, // Automatically remove an already imported chunk
+      defaultExtension: "glsl", // Shader suffix when no extension is specified
+      compress: false, // Compress output shader code
+      watch: true, // Recompile shader on change
+      root: "/", // Directory for root imports
+    }),
+  ],
 });
