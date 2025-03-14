771df2f310d0d3fd1d6e0ab71516a1e20784dfeb
Tim Lai <timlaicodes@gmail.com>
2025-03-14 00:17:46 -0400

    Added new social buttons

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -41,14 +41,58 @@ export const subItems: SubItem[] = [
 
 export const socialButtons: SocialButton[] = [
   {
+    tooltip: "Twitch",
+    url: "https://twitch.tv/timrlai",
+    icon: "cib:twitch",
+    main: false,
+  },
+  {
+    tooltip: "YouTube",
+    url: "https://www.youtube.com/@timrlai",
+    icon: "mingcute:youtube-fill",
+    main: false,
+  },
+  {
+    tooltip: "Artstation",
+    url: "https://artstation.com/timrlai",
+    icon: "cib:artstation",
+    main: false,
+  },
+  {
+    tooltip: "Dribbble",
+    url: "https://dribbble.com/timrlai",
+    icon: "mingcute:dribbble-fill",
+    main: false,
+  },
+  {
+    tooltip: "Instagram",
+    url: "https://instagram.com/timrlai",
+    icon: "mingcute:instagram-line",
+    main: false,
+  },
+  {
+    tooltip: "Threads",
+    url: "https://threads.com/timrlai",
+    icon: "mingcute:threads-fill",
+    main: false,
+  },
+  {
+    tooltip: "Twitter (No, not X)",
+    url: "https://twitter.com/timrlai",
+    icon: "mingcute:twitter-fill",
+    main: true,
+  },
+  {
     tooltip: "GitHub",
     url: "https://github.com/timrlai",
     icon: "mingcute:github-fill",
+    main: true,
   },
   {
     tooltip: "LinkedIn",
     url: "https://www.linkedin.com/in/timrlai/",
     icon: "mingcute:linkedin-fill",
+    main: true,
   },
 ];
 
