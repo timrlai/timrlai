fceaf271010a249c436b4036f01ec3a59650578b
Tim Lai <timlaicodes@gmail.com>
2025-03-18 17:44:50 -0400

    Added router to main.ts and RouterView to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { RouterView } from "vue-router";
 import PageHeader from "./components/Layout/PageHeader.vue";
 import PageFooter from "./components/Layout/PageFooter.vue";
 </script>
 
 <template>
   <PageHeader />
+  <RouterView />
   <PageFooter />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
 import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
+
 import "./css/style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import Home from "./components/Home/Home.vue";
+
+const routes = [{ path: "/", component: Home }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to) {
+    console.log(to.hash);
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 100,
+        behavior: "smooth",
+      };
+    } else {
+      return {
+        top: 0,
+        behavior: "smooth",
+      };
+    }
+  },
+});
+
+createApp(App).use(router).mount("#app");
