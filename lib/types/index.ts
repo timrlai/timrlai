3f146bea1ec5f4afc945be858a01b1663fd6f558
Tim Lai <timlaicodes@gmail.com>
2025-03-19 21:27:58 -0400

    Added and updated lottie shape types

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,4 +1,4 @@
-import type { CanvasTexture, Side } from "three";
+import type { CanvasTexture, Wrapping, Side } from "three";
 
 export type ColorKind =
   | "neutral"
@@ -42,17 +42,51 @@ export type LottieTexture = CanvasTexture & {
   animation?: object;
 };
 
-export type LottieSphereProps = {
+export type LottieShapeProps = {
   src: string;
   repeatX?: number;
   repeatY?: number;
-  radius?: number;
-  segments?: number;
+  wrapS?: Wrapping;
+  wrapT?: Wrapping;
   position?: [number, number, number];
   rotation?: [number, number, number];
   scale?: [number, number, number];
+  renderOrder?: number;
   side?: Side;
+  material?: "basic" | "standard" | "toon";
   transparent?: boolean;
+  depthWrite?: boolean;
+};
+
+export type LottieSphereProps = LottieShapeProps & {
+  radius?: number;
+  segments?: number;
+};
+
+export type LottiePlaneProps = LottieShapeProps & {
+  width?: number;
+  height?: number;
+  widthSegments?: number;
+  heightSegments?: number;
+};
+
+export type LottieRoundedBoxProps = LottieShapeProps & {
+  width?: number;
+  height?: number;
+  depth?: number;
+  segments?: number;
+  radius?: number;
+};
+
+export type LottieCylinderProps = LottieShapeProps & {
+  radiusTop?: number;
+  radiusBottom?: number;
+  height?: number;
+  radialSegments?: number;
+  heightSegments?: number;
+  openEnded?: boolean;
+  thetaStart?: number;
+  theataEnd?: number;
 };
 
 export type CloudCanvasProps = {
