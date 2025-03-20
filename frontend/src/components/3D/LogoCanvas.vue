adb655b9686ecbe17a241817852ea4d2c94340c9
Tim Lai <timlaicodes@gmail.com>
2025-03-20 19:59:33 -0400

    Adjusted 3D canvas poses and settings for mobile

--- frontend/src/components/3D/LogoCanvas.vue
+++ frontend/src/components/3D/LogoCanvas.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { defineProps } from "vue";
+import isMobile from "is-mobile";
 import { TresCanvas } from "@tresjs/core";
 import { OrbitControls, GLTFModel, Text3D } from "@tresjs/cientos";
 import type { LogoCanvasProps } from "../../../lib/types";
@@ -17,6 +18,31 @@ const {
   fontPath = "/fonts/ubuntu_titling/Ubuntu_Titlin_Rg_Bold.json",
   fontSize = 1,
 } = defineProps<LogoCanvasProps>();
+
+const isMobileOrTablet: boolean = isMobile() || isMobile({ tablet: true });
+const width: number = window.innerWidth;
+const height: number = window.innerHeight;
+const isPortrait: boolean = width < 750;
+const isLandscape: boolean = height < 750;
+
+const logoPosition: [number, number, number] = isPortrait
+  ? [-2.3, 6.6, -25]
+  : isLandscape
+    ? [-9, 1, -25]
+    : [-10.5, 1, -25];
+const logoRotation: [number, number, number] = isPortrait
+  ? [0.5, 0, 0]
+  : [0, 0.2, 0];
+const logoScale: number = isPortrait ? 25 : isLandscape ? 50 : 60;
+const taglinePosition: [number, number, number] = isPortrait
+  ? [0, 2, -15]
+  : isLandscape
+    ? [3, 2.7, -15]
+    : [3.5, 3, -15];
+const taglineRotation: [number, number, number] = isPortrait
+  ? [-0.5, 0, 0]
+  : [0, -0.5, 0];
+const taglineScale: number = isPortrait ? 0.7 : isLandscape ? 0.8 : 1;
 </script>
 
 <template>
@@ -28,6 +54,7 @@ const {
     <TresCanvas :clear-color="canvasColor" shadows alpha>
       <TresPerspectiveCamera :position="[0, 0, 1]" />
       <OrbitControls
+        v-if="!(isMobileOrTablet && isLandscape)"
         :minDistance="0"
         :maxDistance="Infinity"
         :minPolarAngle="0"
@@ -42,12 +69,16 @@ const {
         <GLTFModel
           :path="gltfPath"
           draco
-          :scale="60"
-          :position="[-10.5, 1, -25]"
-          :rotation="[0, 0.2, 0]"
+          :position="logoPosition"
+          :rotation="logoRotation"
+          :scale="logoScale"
         />
       </Suspense>
-      <TresMesh :position="[3.5, 3, -15]" :rotation="[0, -0.5, 0]">
+      <TresMesh
+        :position="taglinePosition"
+        :rotation="taglineRotation"
+        :scale="taglineScale"
+      >
         <Suspense
           ><TresMesh :position="[0, 2, 0]"
             ><Text3D :font="fontPath" :size="fontSize"
@@ -74,7 +105,7 @@ const {
         </Suspense>
       </TresMesh>
       <Suspense>
-        <GLCloud />
+        <GLCloud v-if="!isMobileOrTablet" />
       </Suspense>
       <TresAmbientLight
         :position="[0, 10, 0]"
