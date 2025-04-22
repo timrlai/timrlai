0d5e0d73c478c4f2d75b6bb3b249580b6ab5c340
Tim Lai <timlaicodes@gmail.com>
2025-04-21 20:51:23 -0400

    Ensure poses reset on load using watchEffect

--- frontend/src/components/3D/LogoCanvas.vue
+++ frontend/src/components/3D/LogoCanvas.vue
@@ -3,10 +3,9 @@ import {
   type Ref,
   defineProps,
   ref,
-  onActivated,
-  onDeactivated,
   onMounted,
   onUnmounted,
+  watchEffect,
 } from "vue";
 import isMobile from "is-mobile";
 import { TresCanvas } from "@tresjs/core";
@@ -100,15 +99,6 @@ const setPoses = () => {
   canvasKey.value = `logo-canvas-${Math.random()}`;
 };
 
-onActivated(() => {
-  // Reset 3D poses when window loads - necessary for mobile Firefox to detect window width and height
-  window.addEventListener("load", setPoses);
-});
-
-onDeactivated(() => {
-  window.removeEventListener("load", setPoses);
-});
-
 onMounted(() => {
   // Reset 3D poses when window resizes or device is rotated
   window.addEventListener("resize", setPoses);
@@ -116,6 +106,11 @@ onMounted(() => {
 
 onUnmounted(() => {
   window.removeEventListener("resize", setPoses);
+});
+
+watchEffect(() => {
+  // Reset 3D poses when window loads
+  if (window?.innerWidth && window?.innerHeight) setPoses();
 });
 </script>
 
