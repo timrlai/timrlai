1e6d13ca7f9ae76ae5be7957cd454bb3cd55bc83
Tim Lai <timlaicodes@gmail.com>
2025-04-27 23:10:33 -0400

    Added soft skills lottie to Skills component

--- frontend/src/components/Pages/Home/Skills.vue
+++ frontend/src/components/Pages/Home/Skills.vue
@@ -17,7 +17,8 @@ import {
 
 import LottiePlayer from "../../Common/LottiePlayer.vue";
 
-const { AVATAR_SKILLS_DESK_LOTTIE_PATH } = lottieConstants;
+const { AVATAR_SKILLS_DESK_LOTTIE_PATH, AVATAR_SKILLS_SOFT_LOTTIE_PATH } =
+  lottieConstants;
 
 const skillIcon = ref(primarySkills[0].icon);
 const onSkillTyped = (currentTitle: string) => {
@@ -319,35 +320,48 @@ const randomizedSkills = [...primarySkills]
       class="mockup-window bg-secondary text-secondary-content border-4 rounded-t-none border-primary shadow-lg shadow-primary"
     >
       <div class="p-5">
-        <h2 class="text-2xl sm:text-3xl ubuntu-bold mb-4">
-          Soft Skills
-          <Icon icon="fluent-emoji:handshake" class="inline-block" />
-        </h2>
-        <p class="text-xl sm:text-2xl">
-          Having worked with large, fast-paced organizations such as Futuretalk
-          Inc.,
-          <Icon icon="fluent-emoji:robot" class="inline-block" /> Seneca
-          Polytechnic
-          <Icon icon="fluent-emoji:man-teacher-light" class="inline-block" />
-          and the Toronto Animation Arts Festival International,
-          <Icon icon="fluent-emoji:clapper-board" class="inline-block" />
-          I have experience communicating
-          <Icon icon="fluent-emoji:speech-balloon" class="inline-block" />
-          with large teams and working under time constraints.
-          <Icon icon="fluent-emoji:hourglass-done" class="inline-block" />
-          People know me to be passionate,
-          <Icon icon="fluent-emoji:heart-on-fire" class="inline-block" />
-          creative,
-          <Icon icon="fluent-emoji:artist-palette" class="inline-block" />
-          detail-oriented,
-          <Icon icon="fluent-emoji:eye" class="inline-block" />
-          resourceful,
-          <Icon icon="fluent-emoji:light-bulb" class="inline-block" />
-          reliable
-          <Icon icon="fluent-emoji:hundred-points" class="inline-block" /> and
-          adaptable.
-          <Icon icon="fluent-emoji:flexed-biceps-light" class="inline-block" />
-        </p>
+        <div class="flex flex-wrap justify-between items-center">
+          <div class="w-full sm:w-4/5 sm:pr-14">
+            <h2 class="text-2xl sm:text-3xl ubuntu-bold mb-4">
+              Soft Skills
+              <Icon icon="fluent-emoji:handshake" class="inline-block" />
+            </h2>
+            <p class="text-xl sm:text-2xl">
+              Having worked with large, fast-paced organizations such as
+              Futuretalk Inc.,
+              <Icon icon="fluent-emoji:robot" class="inline-block" /> Seneca
+              Polytechnic
+              <Icon
+                icon="fluent-emoji:man-teacher-light"
+                class="inline-block"
+              />
+              and the Toronto Animation Arts Festival International,
+              <Icon icon="fluent-emoji:clapper-board" class="inline-block" />
+              I have experience communicating
+              <Icon icon="fluent-emoji:speech-balloon" class="inline-block" />
+              with large teams and working under time constraints.
+              <Icon icon="fluent-emoji:hourglass-done" class="inline-block" />
+              People know me to be passionate,
+              <Icon icon="fluent-emoji:heart-on-fire" class="inline-block" />
+              creative,
+              <Icon icon="fluent-emoji:artist-palette" class="inline-block" />
+              detail-oriented,
+              <Icon icon="fluent-emoji:eye" class="inline-block" />
+              resourceful,
+              <Icon icon="fluent-emoji:light-bulb" class="inline-block" />
+              reliable
+              <Icon icon="fluent-emoji:hundred-points" class="inline-block" />
+              and adaptable.
+              <Icon
+                icon="fluent-emoji:flexed-biceps-light"
+                class="inline-block"
+              />
+            </p>
+          </div>
+          <div class="w-full sm:w-1/5">
+            <LottiePlayer :src="AVATAR_SKILLS_SOFT_LOTTIE_PATH" autoPlay />
+          </div>
+        </div>
       </div>
     </div>
   </section>
