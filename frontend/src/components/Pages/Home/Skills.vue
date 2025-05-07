ce170e11f584a8bcbd1202d17aaa397cb7d9ca68
Tim Lai <timlaicodes@gmail.com>
2025-05-06 23:39:29 -0400

    Updated heading formatting in Skills

--- frontend/src/components/Pages/Home/Skills.vue
+++ frontend/src/components/Pages/Home/Skills.vue
@@ -50,9 +50,9 @@ const checkboxKeyPress = (event: KeyboardEvent) => {
       class="mockup-window bg-secondary text-secondary-content border-4 border-primary rounded-b-none shadow-lg shadow-primary"
     >
       <h1
-        class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl ubuntu-titling mb-4 px-5"
+        class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl uppercase ubuntu-titling mb-4 px-5"
       >
-        <Icon icon="fluent-color:code-24" class="inline-block h-[1em]" />
+        <Icon icon="mingcute:code-fill" class="inline-block h-[1em]" />
         Skills
       </h1>
     </div>
