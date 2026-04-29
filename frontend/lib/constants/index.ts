1303d627a968c1abb9cf99e93ace1c74051a7f64
Tim Lai <timlaicodes@gmail.com>
2026-04-28 20:13:53 -0400

    Added buttonColors, tooltipColors and tooltipPositions

--- frontend/lib/constants/index.ts
+++ frontend/lib/constants/index.ts
@@ -787,3 +787,30 @@ export const madeWithSkills: Skill[] = [
   { title: "Render", icon: "simple-icons:render" },
   { title: "Porkbun", icon: "simple-icons:porkbun" },
 ];
+
+export const buttonColors = {
+  neutral: "btn-neutral",
+  primary: "btn-primary",
+  secondary: "btn-secondary",
+  accent: "btn-accent",
+  info: "btn-info",
+  success: "btn-success",
+  warning: "btn-warning",
+  error: "btn-error",
+};
+export const tooltipColors = {
+  neutral: "tooltip-neutral",
+  primary: "tooltip-primary",
+  secondary: "tooltip-secondary",
+  accent: "tooltip-accent",
+  info: "tooltip-info",
+  success: "tooltip-success",
+  warning: "tooltip-warning",
+  error: "tooltip-error",
+};
+export const tooltipPositions = {
+  left: "tooltip-left",
+  right: "tooltip-right",
+  top: "tooltip-top",
+  bottom: "tooltip-bottom",
+};
