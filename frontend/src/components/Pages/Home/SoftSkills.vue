396f3a138028a75a0e469bd84ed9143bd53b11bc
Tim Lai <timlaicodes@gmail.com>
2026-06-09 23:48:56 -0400

    Converted lottie to scrolling, added work philosophy

--- frontend/src/components/Pages/Home/SoftSkills.vue
+++ frontend/src/components/Pages/Home/SoftSkills.vue
@@ -23,53 +23,42 @@ const { isNight } = storeToRefs(store);
     class="mockup-window bg-secondary/80 text-secondary-content border-4 rounded-t-none border-primary shadow-lg shadow-primary"
   >
     <div class="p-5">
-      <div class="flex flex-wrap justify-between">
-        <div class="w-full sm:w-3/4 sm:pr-14">
-          <h2 class="text-3xl sm:text-4xl ubuntu-titling mb-4">
+      <div class="flex flex-wrap justify-between items-center">
+        <div class="w-full sm:w-1/2 sm:pr-14">
+          <h2 class="text-3xl sm:text-4xl ubuntu-titling mb-8">
             <Icon icon="fluent-emoji:handshake" class="inline-block h-[1em]" />
             Soft Skills
           </h2>
-          <p class="text-xl sm:text-2xl">
+          <h3 class="text-2xl sm:text-3xl ubuntu-titling mb-4">Teamwork</h3>
+          <p class="text-xl sm:text-2xl mb-8 text-justify">
             Having worked with large, fast-paced organizations such as
-            Futuretalk Inc.,
-            <Icon icon="fluent-emoji:robot" class="inline-block" /> Seneca
-            Polytechnic
-            <Icon icon="fluent-emoji:man-teacher-light" class="inline-block" />
-            and the Toronto Animation Arts Festival International,
-            <Icon icon="fluent-emoji:clapper-board" class="inline-block" />
-            I have experience communicating
-            <Icon icon="fluent-emoji:speech-balloon" class="inline-block" />
-            with large teams and working under time constraints.
-            <Icon icon="fluent-emoji:hourglass-done" class="inline-block" />
-            People know me to be passionate,
-            <Icon icon="fluent-emoji:heart-on-fire" class="inline-block" />
-            creative,
-            <Icon icon="fluent-emoji:artist-palette" class="inline-block" />
-            detail-oriented,
-            <Icon icon="fluent-emoji:eye" class="inline-block" />
-            resourceful,
-            <Icon icon="fluent-emoji:light-bulb" class="inline-block" />
-            reliable
-            <Icon icon="fluent-emoji:hundred-points" class="inline-block" />
-            and adaptable.
-            <Icon
-              icon="fluent-emoji:flexed-biceps-light"
-              class="inline-block"
-            />
+            Futuretalk Inc., Seneca Polytechnic and the Toronto Animation Arts
+            Festival International, I have experience communicating with large
+            teams and working under time constraints. People know me to be
+            passionate, creative, detail-oriented, resourceful, reliable and
+            adaptable.
+          </p>
+          <h3 class="text-2xl sm:text-3xl ubuntu-titling mb-4">
+            Work Philosophy Statement
+          </h3>
+          <p class="text-xl sm:text-2xl text-justify">
+            As a developer who is also an artist and educator, I approach work
+            with creativity and curiosity, adapting to meet the team's unique
+            needs and articulating complex technical concepts to varying
+            stakeholders.
           </p>
         </div>
-        <div v-if="!isNight" class="w-full sm:w-1/4">
+        <div class="w-full sm:w-1/2 min-h-75">
           <Suspense>
             <LottiePlayer
-              :src="AVATAR_SKILLS_SOFT_LOTTIE_PATH"
-              autoPlay
-              v-once
+              :src="
+                isNight
+                  ? BAT_SKILLS_SOFT_LOTTIE_PATH
+                  : AVATAR_SKILLS_SOFT_LOTTIE_PATH
+              "
+              scrolling
+              target="soft-skills-lottie"
             />
-          </Suspense>
-        </div>
-        <div v-if="isNight" class="w-full sm:w-1/4">
-          <Suspense>
-            <LottiePlayer :src="BAT_SKILLS_SOFT_LOTTIE_PATH" autoPlay v-once />
           </Suspense>
         </div>
       </div>
