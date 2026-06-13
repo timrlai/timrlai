f81d9589aabd68ad16a1d4255458cdd1dfb8c0e5
Tim Lai <timlaicodes@gmail.com>
2026-06-13 09:27:59 -0400

    Created Logo component

---
+++ frontend/src/components/3D/Logo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { LogoProps } from "../../../lib/types/index.ts";
+
+const { position, rotation, scale, isNight, lightModel, darkModel } =
+  defineProps<LogoProps>();
+</script>
+
+<template>
+  <TresGroup :position="position" :rotation="rotation" :scale="scale">
+    <Suspense><primitive v-if="!isNight" :object="lightModel" /></Suspense>
+    <Suspense><primitive v-if="isNight" :object="darkModel" /></Suspense>
+  </TresGroup>
+</template>
