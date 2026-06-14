b059614c65da3a6567016e9e1226293e5c5aa3a4
Tim Lai <timlaicodes@gmail.com>
2026-06-13 21:43:06 -0400

    Update LogoCanvasCamera to use Rapier physics

--- frontend/src/components/3D/LogoCanvasCamera.vue
+++ frontend/src/components/3D/LogoCanvasCamera.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import { onMounted, onBeforeUnmount, nextTick, ref } from "vue";
-import { useTresContext } from "@tresjs/core";
+import { Mesh } from "three";
+import { useLoop, useTresContext } from "@tresjs/core";
 import { OrbitControls } from "@tresjs/cientos";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {
+  ColliderDesc,
+  RigidBodyDesc,
+  RigidBodyType,
+} from "@dimforge/rapier3d-compat";
 
+import type { LogoCanvasCameraProps } from "../../../lib/types/index.ts";
 import useScrollTriggers from "../../../lib/gsap/useScrollTriggers.ts";
+import useRapier from "../../../lib/rapier/useRapier.ts";
 
 const {
   sectionId,
@@ -14,14 +22,8 @@ const {
   isMobileOrTablet,
   verticalRotationLimit,
   horizontalRotationLimit,
-} = defineProps<{
-  sectionId: string;
-  scrollTrackId: string;
-  isLandscape: boolean;
-  isMobileOrTablet: boolean;
-  verticalRotationLimit: number;
-  horizontalRotationLimit: number;
-}>();
+  meshes,
+} = defineProps<LogoCanvasCameraProps>();
 
 const { camera } = useTresContext();
 
@@ -33,25 +35,54 @@ const {
   killAllScrollTriggers,
   buildThreeScrollTriggerConfig,
 } = useScrollTriggers();
-
+let logoDropped = false;
 const orbitRef = ref<typeof OrbitControls>();
+
+const { initRapier, step, addRigidBody } = useRapier();
+const { onRender } = useLoop();
 
 onMounted(async () => {
   await nextTick();
 
   if (orbitRef.value?.instance) orbitRef.value.instance.enabled = false;
 
+  await initRapier();
+
+  const mesh = new Mesh();
+  const floorDesc = RigidBodyDesc.fixed().setTranslation(0, -0.08, 0);
+  const floorCollider = ColliderDesc.cuboid(10, 0.05, 10);
+
+  addRigidBody(mesh, floorDesc, floorCollider);
+
   const timelineSettings: gsap.TimelineVars = {
     scrollTrigger: buildThreeScrollTriggerConfig(
       sectionId,
       scrollTrackId,
       (self: ScrollTrigger) => {
+        if (self.progress > 0.99 && !logoDropped && meshes) {
+          logoDropped = true;
+          meshes.forEach((mesh) => {
+            const desc = RigidBodyDesc.kinematicPositionBased();
+            const points = new Float32Array(
+              mesh.geometry.attributes.position.array,
+            );
+            const collider = ColliderDesc.convexHull(points);
+            if (!collider) return;
+            setTimeout(() => {
+              const body = addRigidBody(mesh, desc, collider);
+              if (!body) return;
+              body.setBodyType(RigidBodyType.Dynamic, true);
+              body.applyImpulse({ x: 0, y: -0.5, z: 0.5 }, true);
+            }, 1000);
+          });
+        }
         if (
           !isMobileOrTablet &&
           orbitRef.value?.instance &&
           self.progress === 1
-        )
+        ) {
           orbitRef.value.instance.enabled = true;
+        }
       },
     ),
   };
@@ -89,6 +120,10 @@ onMounted(async () => {
   }
 
   refreshScrollTriggers();
+});
+
+onRender(({ delta }: { delta: number }) => {
+  step(delta);
 });
 
 onBeforeUnmount(() => killAllScrollTriggers(scrollTriggers));
